b60151302b0e5f7f9098b3c148bd1e4f21e93796
r.hosoya <r.hosoya@core-tech.jp>
2023-08-06 11:50:45 +0900

    モデル・コントローラ・マイグレーション・シーダーを作成

---
+++ laravel-project/app/Http/Controllers/ContentController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Services\ContentService;
+use Facade\FlareClient\View;
+use Illuminate\Http\Request;
+use App\Http\Requests\ContentRequest;
+
+class ContentController extends Controller
+{
+    // public function __construct(ContentService $contentService)
+    // {
+    //     this->contentService = $contentService;
+    // }
+
+    /**
+     * 投稿ページ一覧の表示
+     * @return view
+     * @author hosoya
+     */
+    public function list()
+    {
+        // $content_list = $this->conte;
+    }
+}
