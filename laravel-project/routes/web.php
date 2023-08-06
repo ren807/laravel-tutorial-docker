3bbd71f0ea0e41b056554c9aadf4853c034fb7ec
r.hosoya <r.hosoya@core-tech.jp>
2023-08-06 11:12:29 +0900

    ルーティングの実装

--- laravel-project/routes/web.php
+++ laravel-project/routes/web.php
@@ -33,4 +33,12 @@ Route::resource('chirps', ChirpController::class)
 	->only(['index', 'store', 'edit', 'update', 'destroy'])
 	->middleware(['auth', 'verified']);
 
+Route::prefix('contents')->middleware('auth')->group(function() {
+	Route::get('/list', [ContentController::class, 'list'])->name('contents.list');
+	Route::get('/create', [ContentController::class, 'create'])->name('contents.create');
+	Route::get('/update/{content_id}', [ContentController::class, 'update'])->name('contents.update');
+	Route::get('/delete/{content_id}', [ContentController::class, 'delete'])->name('contents.delete');
+	Route::post('/save', [ContentController::class, 'save'])->name('contents.save');
+});
+
 require __DIR__.'/auth.php';
