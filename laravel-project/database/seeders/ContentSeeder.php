28d1c15ad7c85268ab0887294567511ac13fc4a9
r.hosoya <r.hosoya@core-tech.jp>
2023-08-06 12:32:00 +0900

    シーダーを作成

--- laravel-project/database/seeders/ContentSeeder.php
+++ laravel-project/database/seeders/ContentSeeder.php
@@ -2,16 +2,39 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Carbon\Carbon;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class ContentSeeder extends Seeder
 {
     /**
      * Run the database seeds.
+     *
+     * @return void
      */
-    public function run(): void
+    public function run()
     {
-        //
+        $contents = [
+            'テスト投稿_1',
+            'テスト投稿_2',
+            'テスト投稿_3',
+            'テスト投稿_4',
+            'テスト投稿_5',
+            'テスト投稿_6',
+            'テスト投稿_7',
+            'テスト投稿_8',
+        ];
+
+        $now = Carbon::now();
+        foreach ($contents as $content) {
+            $info = [
+                'content' => $content,
+                'deleted_flag' => 0,
+                'created_at' => $now,
+                'updated_at' => $now,
+            ];
+            DB::table('contents')->insert($info);
+        }
     }
 }
